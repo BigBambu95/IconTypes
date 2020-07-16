f09a2941297eec29d09f10f5794864924bd2de99
unknown <hjot@yandex.ru>
2020-07-16 09:47:01 +0500

    Типизация названия иконок

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const IconNames = {
+  Arrow: "Arrow" as const,
+  Alert: "Alert" as const,
+  ArrowV: "Arrow-V" as const,
+} 
+
+type ReturnType<T> = T extends {[key: string]: infer U} ? U : any;
+
+type IconProps = {
+  name: ReturnType<typeof IconNames>;
+  age?: number;
+}
+
+const Icon = (props: IconProps) => {
+  return(
+    <div>
+      {props.name}
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Icon name="Arrow-V" />
     </div>
   );
 }
